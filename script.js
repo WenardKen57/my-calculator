4b774734b32c6757fbd0871eb614a108a181f03f
Wenard Santos <wenardken57@gmail.com>
2025-03-06 22:24:28 +0800

    Add basic arithmetic functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log("Hello world!");
+function add(x, y) {
+  return (x + y);
+}
+
+function subtract(x, y) {
+  return (x - y);
+}
+
+function multiply(x, y) {
+  return (x * y);
+}
+
+function divide(x, y) {
+  return (x / y);
+}
+
+console.log(add(1, 3));
+console.log(subtract(2, 4))
+console.log(multiply(0, 6))
+console.log(divide(1, -3))
